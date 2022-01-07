f53f8a79bbec8417e845453a3da68ac8b16945af
MYKYTA VOSTRIKOV <mykytavostrikov@gmail.com>
2022-01-07 09:09:38 +0200

    hw-19

---
+++ HomeWork_19/src/app/utils.js
@@ -0,0 +1,88 @@
+const COOKIE = (() => {
+
+   const getCookie = (name) => {
+
+      let matches = document.cookie.match(new RegExp(
+
+         "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+
+      ));
+
+      return matches ? decodeURIComponent(matches[1]) : undefined;
+
+   };
+
+
+   const hasCookie = (name) => {
+
+      return Boolean(getCookie(name));
+
+   };
+
+   const setCookie = (name, value, options = {}) => {
+
+      options = {
+
+         path: '/',
+
+         ...options
+
+      };
+
+      if (options.expires instanceof Date) {
+
+         options.expires = options.expires.toUTCString();
+
+      };
+
+      let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+      for (let optionKey in options) {
+
+         updatedCookie += "; " + optionKey;
+
+         let optionValue = options[optionKey];
+
+         if (optionValue !== true) {
+
+            updatedCookie += "=" + optionValue;
+
+         };
+
+      };
+
+      document.cookie = updatedCookie;
+
+   };
+
+   const removeCookie = (name) => {
+
+      setCookie(name, '', {
+
+         'max-age': -1
+
+      });
+
+   };
+
+   const count = () => {
+
+      return document.cookie.split(';').length;
+
+   };
+
+   return {
+
+      getCookie,
+
+      hasCookie,
+
+      setCookie,
+
+      removeCookie,
+
+      count,
+
+   };
+
+})();
